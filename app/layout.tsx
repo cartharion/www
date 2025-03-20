922e5bcf9ee9de0975814248d89dca6c5329313b
ashgw <ashrefgw@proton.me>
2025-03-20 13:07:58 +0100

    add styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
 
-// import './styles/globals.css';
+import './styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
