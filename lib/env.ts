0517c6407d43ecf31258d3efcc19dd2899763df8
ashgw <ashrefgw@proton.me>
2025-03-20 17:11:48 +0100

    skip validation on the browser for `@ashgw/ts-env`

--- lib/env.ts
+++ lib/env.ts
@@ -1,6 +1,8 @@
 import { createEnv, preset } from '@ashgw/ts-env';
 import { z } from 'zod';
 
+const isBrowser = typeof window !== undefined;
+
 export const env = createEnv({
   vars: {
     CLOUDFLARE_ANALYTICS_TOKEN: z.string().min(20),
@@ -9,5 +11,5 @@ export const env = createEnv({
   },
   disablePrefix: ['NODE_ENV'],
   prefix: 'NEXT_PUBLIC',
-  skipValidation: typeof window === undefined, // skip validation on client side
+  skipValidation: isBrowser, // skip validation on client side
 });
